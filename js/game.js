08e60825c83e4bfe08ca5b73bc2d512c16453c5c
Picabea <iniakipicabea@gmail.com>
2023-12-14 22:10:07 -0300

    Se implementa el uso de fetch importando informacion de un JSON y descargando informacion de una API. Se agrega el uso de SweetAlert 2. Se cambia el sistema para generar el numero aleatorio por completo. Se soluciona un bug que no guardaba la partida en el historial al ultimo intento

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 const flechaAbajo = "../images/flechaAbajo.png"
 const flechaArriba = "../images/flechaArriba.png"
 const trofeo = "../images/trofeo.png"
-
+const libro = "../images/bookIcon.png"
 //Elementos del HTML
 const inputNumeros = document.querySelector("#inputNumeros")
 const botonNumeros = document.querySelector("#botonNumeros")
@@ -10,12 +10,18 @@ const output = document.querySelector("output")
 const selectDificultad = document.querySelector("select")
 const buttonNewGame = document.querySelector("#buttonNewGame")
 const section = document.querySelector(`#sectionIntentos`)
+const submitButton = document.querySelector("body div div button")
+const helpImg = document.querySelector("#helpButton")
+
 //Creamos el objeto del session storage que guarda el num de intentos
+let data = []
 let numeroIngresado = null
 let intentos = 0
 let dificultad = 10
 let game_status = sessionStorage.getItem("game_status")
 let id = null
+let numero = null
+let numeroInput = null
 
 //Creacion de cada objeto representativo de una partida con sus ctas.
 function Partida(resultado, id){
@@ -27,7 +33,8 @@ function Partida(resultado, id){
 }
 
 //Eventos del DOM para ejecutar el codigo
-inputNumeros.addEventListener("change", comprobarNumero)//Cuando se ingresa un numero ejecuta la comprobacion y carga el div
+//inputNumeros.addEventListener("change", comprobarNumero)//Cuando se ingresa un numero ejecuta la comprobacion y carga el div
+submitButton.addEventListener("click", recuperarNumero)
 
 selectDificultad.addEventListener("change", (evento) => {
     dificultad = evento.target.value
@@ -36,46 +43,114 @@ selectDificultad.addEventListener("change", (evento) => {
 
 buttonNewGame.addEventListener("click", iniciarJuego)//Sirve para reiniciar el juego, utiliza la misma funcion que establece todos los valores iniciales apenas carga la pagina
 
+helpImg.addEventListener("click", helpAlert)
+
 document.addEventListener("DOMContentLoaded", iniciarJuego)//Cuando carga la pagina reinicia todas las variables y setea el juego
 
-function generarNumeroAleatorio(min, max){//Devuelve un numero aleatorio
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1) + min);
+
+
+
+function helpAlert(evento){
+    Swal.fire({
+        imageUrl: libro,
+        imageHeight: 100,
+        imageWidth: 100,
+        html: "<h3>Reglas</h3>Tienes que ingresar un número entre <b>1</b> y <b>100</b>. El sistema te dirá si el número ingresado es <b>mayor</b> o <b>menor</b>. El objetivo es adivinar el número escogido aleatoriamente. <h3>Dificultad</h3>Se puede elegir entre 3 dificultades <b>fácil</b> (10 intentos), <b>media</b> (5 intentos) y <b>difícil</b> (3 intentos)"
+      });
 }
 
-function comprobarNumero(evento){
-    selectDificultad.disabled = true //Desactiva la posiiblidad de cambiar la dificultad una vez iniciado el juego
+async function generarNumeroAleatorio(){//Devuelve un numero aleatorio
+    let data = []
 
-    if(sessionStorage.getItem("game_status")){ //Comprobar que el juego siga activo
-        if(Number(sessionStorage.getItem("intentos")) < Number(sessionStorage.getItem("dificultad"))){ //comprobar que sigan habiendo intentos disponibles
+    fetch("../randomNumberParameters.json")
+        .then((res) => {return res.json()})
+        .then((contenidoJson) => {return fetch(`${contenidoJson.url}?num=1&min=${contenidoJson.min}&max=${contenidoJson.max}&col=${contenidoJson.col}&base=${contenidoJson.base}&format=${contenidoJson.format}&rnd=${contenidoJson.rnd}&apiKey=${contenidoJson.key}`)})
+        .then((resNumero) => {return resNumero.text()})
+        .then((numeroaleatorio) => {
+            sessionStorage.setItem("numero", Number(numeroaleatorio))
+        })
+        .catch((error) => {console.log(error)})
+}
 
-            //actualizar los intentos
-            sessionStorage.setItem("intentos", Number(sessionStorage.getItem("intentos")) + 1)
 
-            //cargar la lista de numeros ingresados para cargar en el historial
-            numeroIngresado = Number(evento.target.value)
-            numerosIngresados = JSON.parse(sessionStorage.getItem("numerosIngresados"))
-            numerosIngresados.push(numeroIngresado)
-            sessionStorage.setItem("numerosIngresados", JSON.stringify(numerosIngresados))
+function recuperarNumero(evt){
+    numeroInput = document.querySelector("#inputNumeros").value
+    document.querySelector("#inputNumeros").value = ""
+    numeroInput <= 100 & numeroInput >= 1 
+    ?actualizarVariables(numeroInput) 
+    :Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "El numero ingresado debe ser entre 1 y 100 inclusive",
+      });
+}
 
-            //Agarrar del session storage el numero aleatorio
-            numero = sessionStorage.getItem("numero")
-
-            //Comprobar si el numero es igual, mayor o menor
-            if (numeroIngresado == numero){
-                cargarInformacionDiv(trofeo, numeroIngresado)
-                terminarJuego("ganado")
-            } else if(numeroIngresado > numero){
-                cargarInformacionDiv(flechaAbajo, numeroIngresado)
-            } else{
-                cargarInformacionDiv(flechaArriba, numeroIngresado)
-            }
-        } else{ //En caso de que no hayan mas intentos
-            console.log("No hay mas intentos")
-            terminarJuego("perdido")
+function actualizarVariables(numeroIngresado){
+    selectDificultad.disabled = true //Desactiva la posiblidad de cambiar la dificultad una vez iniciado el juego
+    game_status = sessionStorage.getItem("game_status")
+    intentos = Number(sessionStorage.getItem("intentos"))
+    intentosActualizados = intentos + 1
+    dificultad = Number(sessionStorage.getItem("dificultad"))
+    if(game_status){
+        if(intentos < dificultad){
+        //actualizar los intentos
+        sessionStorage.setItem("intentos", intentosActualizados)
+        
+        //cargar la lista de numeros ingresados para cargar en el historial
+        numeroIngresado = numeroIngresado
+        numerosIngresados = JSON.parse(sessionStorage.getItem("numerosIngresados"))
+        numerosIngresados.push(numeroIngresado)
+        sessionStorage.setItem("numerosIngresados", JSON.stringify(numerosIngresados))
+        comprobarNumero(numeroIngresado, intentosActualizados, dificultad)
         }
     }
+}
+
+
+function comprobarNumero(numeroIngresado, intentos, dificultad){
+     //Agarrar del session storage el numero aleatorio
+     numero = sessionStorage.getItem("numero")
+     console.log(numero)
+
+     //Comprobar si el numero es igual, mayor o menor
+     numeroIngresado = numeroIngresado
+     if (numeroIngresado == numero){//Si se adivina el numero 
+        //Actualizar DOM
+        cargarInformacionDiv(trofeo, numeroIngresado)
+
+        //Enviar alerta
+         Swal.fire({
+             imageUrl: trofeo,
+             imageHeight: 238,
+             imageWidth: 202,
+             title: "Felicitaciones!",
+             text: `Has ganado, el numero era ${numero}`
+         })
+
+         terminarJuego("ganado")
+     }else{
+        //Si el numero no es adivinado actualiza el DOM
+        if(numeroIngresado > numero){
+         cargarInformacionDiv(flechaAbajo, numeroIngresado)
+         } else{
+         cargarInformacionDiv(flechaArriba, numeroIngresado)
+        }
+        console.log(intentos + " " + dificultad)
+        if(intentos === dificultad){//Si ese fue el ultimo intento termina la partida
+            console.log("Termino")
+            Swal.fire({
+                imageUrl: "../images/sadFaceIcon.png",
+                imageHeight: 200,
+                imageWidth: 200,
+                title: "Perdiste!",
+                text: `Pero no pierdas el animo, intenta de nuevo!`
+            })
+            terminarJuego("perdido")
+        }
+        
+    }
+        
+
 
 }
 
@@ -98,7 +173,7 @@ function terminarJuego(resultado){
     //Desactivar la posibilidad de que se sigan cargando numeros
     game_status = false 
     inputNumeros.disabled = true
-    
+    submitButton.disabled = true
     id = Number(localStorage.getItem("id"))
 
     historial = JSON.parse(localStorage.getItem("historial"))//Agarrar del local storage el historial para cargarle un nuevo objeto
@@ -111,6 +186,7 @@ function iniciarJuego(){
     //Posibilitar que se carguen numeros nuevamente
     selectDificultad.disabled = false
     inputNumeros.disabled = false
+    submitButton.disabled = false
 
     //En caso de que nunca se haya creado el objeto historial crearlo
     if (!localStorage.getItem("historial")){
@@ -126,14 +202,10 @@ function iniciarJuego(){
     sessionStorage.setItem("dificultad", 10)
     sessionStorage.setItem("numerosIngresados", JSON.stringify([]))
     sessionStorage.setItem("game_status", true)
-    numero = sessionStorage.setItem("numero", generarNumeroAleatorio(1, 100)) //Generar un nuevo numero aleatorio
+    generarNumeroAleatorio() //Generar un nuevo numero aleatorio
 
     //Eliminar los div del juego anterior
     section.innerHTML = ""
-
-
-
-    console.log(sessionStorage.getItem("numero"))
 }
 
 
